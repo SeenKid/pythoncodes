35eb65a6d4f2ade219464b1de80772280eef7181
SeenKid <30565751+SeenKid@users.noreply.github.com>
2021-12-18 01:11:16 +0100

    Update askageandname1.py

--- askageandname1.py
+++ askageandname1.py
@@ -4,6 +4,8 @@ prenom = str(input("Quel est ton prénom ? "))
 
 phrase = str("Vous avez" + " " + str(age) + " " + "ans, vous aurez" + " " + str(age2) + " " + "ans l'année prochaine et votre nom est " + prenom + ".")
 phrase2 = str("Vous avez {} ans, vous aurez {} ans l'année prochaine et votre nom est {}.".format(age, age2, prenom))
+phrase3 = str(f"Vous avez {age} ans, vous aurez {age2} ans l'année prochaine et votre nom est {prenom}."
 
 print(phrase)
 print(phrase2)
+print(phrase3)
